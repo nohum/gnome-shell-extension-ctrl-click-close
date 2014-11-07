ccee9cc114479874d32b9a7577fed736ce651e06
nohum <eiskalterengel@truthfactory.tk>
2014-11-07 21:19:44 +0100

    bump version for next release

--- ctrl-click-close@nohum.github.com/extension.js
+++ ctrl-click-close@nohum.github.com/extension.js
@@ -1,7 +1,7 @@
 /**
  * CTRL+Click closes windows in window overview.
  * 
- * Version 1.0.2
+ * Version 1.0.3
  *
  * by Wolfgang Gaar
  * http://github.com/nohum/gnome-shell-extension-middle-click-overview
